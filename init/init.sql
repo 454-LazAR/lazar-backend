9765f8e96441d71a118195e2142018658b12a0c4
Drew Paterno <drewpaterno7@gmail.com>
2023-03-27 00:23:44 -0500

    initialize schemas on docker startup

---
+++ init/init.sql
@@ -0,0 +1,19 @@
+
+CREATE DATABASE lazardb;
+
+USE lazardb;
+
+CREATE TABLE games (
+	id INT PRIMARY KEY,
+	gameStatus ENUM('IN_PROGRESS', 'IN_LOBBY', 'FINISHED')
+);
+
+CREATE TABLE players (
+	id INT PRIMARY KEY,
+	gameId INT,
+	username VARCHAR(30),
+	health INT,
+	isAdmin BOOLEAN,
+	-- GPS Data
+	FOREIGN KEY (gameId) REFERENCES Games(id)
+);
