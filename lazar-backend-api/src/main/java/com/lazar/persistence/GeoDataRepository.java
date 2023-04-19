aead7f9ee8396de3e01a2ad843ed944725d70911
Drew Paterno <drewpaterno7@gmail.com>
2023-04-19 14:41:31 -0500

    only get recent data for hitCheck

--- lazar-backend-api/src/main/java/com/lazar/persistence/GeoDataRepository.java
+++ lazar-backend-api/src/main/java/com/lazar/persistence/GeoDataRepository.java
@@ -15,6 +15,8 @@ import java.time.Instant;
 import java.util.List;
 import java.util.Properties;
 
+import static com.lazar.core.GameEventService.TIMEOUT;
+
 @Repository
 public class GeoDataRepository {
 
@@ -25,9 +27,11 @@ public class GeoDataRepository {
     private Properties queries;
 
     public List<GeoData> getGeoDataForHitCheck(GeoData shooterData) {
+        Timestamp minTime = Timestamp.from(Instant.now().minusMillis(TIMEOUT));
         return jdbi.withHandle(h -> h.createQuery(queries.getProperty("geoData.get.in.range"))
                 .bind("gameId", shooterData.getGameId())
                 .bind("playerId", shooterData.getPlayerId())
+                .bind("minTime", minTime)
                 .map((r,c) -> new GeoData(r.getString(1), r.getString(2), r.getString(3), r.getString(4), r.getString(5), r.getString(6)))
                 .list());
     }
